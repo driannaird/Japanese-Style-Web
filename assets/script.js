725b5176ad2e06b7be87b541763201050a3b52ee
Drian-key <riskidrian1@gmail.com>
2022-06-09 15:54:01 +0700

    fix transisi terlalu cepat

--- assets/script.js
+++ assets/script.js
@@ -32,7 +32,8 @@ window.addEventListener("scroll", () => {
     for(let i = 0; i < elements.length; i++){
         let heightWindow = window.innerHeight;
         let topElement = elements[i].getBoundingClientRect().top;
-        if(topElement < heightWindow){
+        let scroll = 200;
+        if(topElement < heightWindow - scroll){
             elements[i].classList.add("transisi");
         }else{
             elements[i].classList.remove("transisi");
